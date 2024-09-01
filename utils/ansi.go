bad33744991a76cea6016c8bb86cddeeec8c0902
Roberto Albanesi <beto0607@gmail.com>
2024-09-01 18:09:20 +0200

    Add utils folder. Add ansi utils

---
+++ utils/ansi.go
@@ -0,0 +1,13 @@
+package utils
+
+func MakeBold(content string) string {
+	return "\033[1m" + content + "\033[0m"
+}
+
+func MakeItalic(content string) string {
+	return "\033[3m" + content + "\033[0m"
+}
+
+func MakeUndeline(content string) string {
+	return "\033[4m" + content + "\033[0m"
+}
